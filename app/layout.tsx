0f25988e7a033b78dda4949858a6906eab8dc9b6
Amir Eliz <amir.eliz155@gmail.com>
2024-02-05 01:26:56 +0200

    Update description in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,8 @@ import './styles/globals.scss'
 
 export const metadata: Metadata = {
 	title: 'CSS Panel',
-	description: '...',
+	description:
+		'CSSharp Panel is a web panel for CounterStrikeSharp that includes many features such as servers management, admins management, ban & comms management, rcon functions and much more, with a modern UI and a lot of customization options.',
 }
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
